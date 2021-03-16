1bcfc42ca27c5816e3ca2cb904cad3eb3b9b9f7a
anirudhallika <62088790+anirudhallika@users.noreply.github.com>
2021-03-16 19:00:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-  <script>
+
 	window.fwSettings={
 	'widget_id':82000002265
 	};
 	!function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
-</script>
 <script type='text/javascript' src='https://ind-widget.freshworks.com/widgets/82000002265.js' async defer></script>
